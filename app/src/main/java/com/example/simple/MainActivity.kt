cb8125642b2a1d05da228fa9077eb8c44498c22f
ether <onyii2313@gmail.com>
2021-05-06 15:13:29 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/simple/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.simple
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.simple.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var drawerLayout: DrawerLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+
+
+        drawerLayout = binding.drawerLayout
+
+
+
+
+        val navController = this.findNavController(R.id.my_nav_host_fragment)
+
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
+
+
+    }
+
+
+}
